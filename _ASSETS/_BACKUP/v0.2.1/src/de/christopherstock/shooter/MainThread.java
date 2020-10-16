0cb172ba5653c8460fcbfe2ff99cf76aabfc33da
Spuffyxe <72823678+Spuffyxe@users.noreply.github.com>
2020-10-16 08:34:03 -0400

    _ASSETS/_BACKUP/v0.2.1/src/de/christopherstock/shooter/MainThread.java
    JAVASCRIPT ONLY!!! USE FOR 3D MOVEMENT ONLY! Good Luck

---
+++ _ASSETS/_BACKUP/v0.2.1/src/de/christopherstock/shooter/MainThread.java
@@ -0,0 +1,3 @@
+//check keys and mouse for lwjgl
+                    if ( ShooterSettings.ENGINE_3D == Engine3D.LWJGL ) LWJGLKeys.checkKeys();
+                    if ( ShooterSettings.ENGINE_3D == Engine3D.LWJGL ) LWJGLMouse.checkMouse();
